3d92c4c122eb264958711aa78e84ffc08be35be7
Ngan <nganbui0330@gmail.com>
2024-08-05 13:01:44 -0400

    h

--- map.php
+++ map.php
@@ -6,7 +6,7 @@ include "includes/header.php";
 <script src="trivia.js">
 <img src="images/WorldMap.png" alt="world map" usemap="#worldmap">
     <map name="worldmap" id="worldmap" >
-        <area coords="130, 20, 500, 200"  href="#" shape="rect" onclick="clickRegion('northAmerica')">
+        <area coords="130, 20, 500, 200"  href="#" shape="rect">
         <area coords="200, 200, 450, 450" href="#" shape="rect">
         <area coords="470, 150, 650, 420" href="#" shape="rect">
         <area coords="670, 40, 920, 290"  href="#" shape="rect">
