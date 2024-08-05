9ede6d5568acb5caee7a3c714f28d46ab50472b8
Ngan <nganbui0330@gmail.com>
2024-08-05 12:45:02 -0400

    foot

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,6 @@
 <br><br><br><br>
+<div style="text-align: center; color: lightGray; font-size: small;">
+Copyright MCA 2024
+</div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -8,12 +8,18 @@ include "includes/header.php";
 
 <img src="images/WorldMap.png" alt="world map" usemap="#worldmap">
     <map name="worldmap" id="worldmap" >
-        <area coords="130, 20, 500, 200" href="#" shape="rect" onclick="clickRegion('northAmerica')">
-        <area coords="200, 200, 450, 450" href="#" shape="rect" onclick="clickRegion('southAmerica')">
-        <area coords="470, 150, 650, 420" href="#" shape="rect" onclick="clickRegion('southAmerica')">
-        <area coords="670, 40, 920, 290" href="#" shape="rect" onclick="clickRegion('southAmerica')">
-        <area coords="850, 300, 990,500" href="#" shape="rect" onclick="clickRegion('southAmerica')">
-        <area coords="500, 15, 675, 150" href="#" shape="rect" onclick="clickRegion('southAmerica')">
+        <area coords="130, 20, 500, 200" href="#" shape="rect" > 
+        // onclick="clickRegion('northAmerica')"
+        <area coords="200, 200, 450, 450" href="#" shape="rect" >
+        //onclick="clickRegion('southAmerica')"
+        <area coords="470, 150, 650, 420" href="#" shape="rect" >
+        //onclick="clickRegion('southAmerica')"
+        <area coords="670, 40, 920, 290" href="#" shape="rect" >
+        //onclick="clickRegion('southAmerica')"
+        <area coords="850, 300, 990,500" href="#" shape="rect" >
+        //onclick="clickRegion('southAmerica')"
+        <area coords="500, 15, 675, 150" href="#" shape="rect" >
+        //onclick="clickRegion('southAmerica')"
     </map>
 
 <?php
