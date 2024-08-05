ad5c46cf97527ba1b96e870db2495edf2dfab78a
Camila <urenacamila0@gmail>
2024-08-05 12:55:35 -0400

    start button 2

--- index.php
+++ index.php
@@ -2,15 +2,10 @@
 include "includes/header.php";
 ?>
 
-<img src="images/startbutton.png" alt="start button">
-    <map name="worldmap" id="worldmap" >
-        <area coords="130, 20, 500, 200" href="region.php?region_id=21" shape="rect">
-        <area coords="200, 200, 450, 450" href="region.php?region_id=13" shape="rect">
-        <area coords="470, 150, 650, 420" href="region.php?region_id=15" shape="rect">
-        <area coords="670, 40, 920, 290" href="region.php?region_id=30" shape="rect">
-        <area coords="850, 300, 990,500" href="region.php?region_id=9" shape="rect">
-        <area coords="500, 15, 675, 150" href="region.php?region_id=151" shape="rect">
-    </map>
+<a href="map.php" >
+    <img id="start" src="images/startbutton.png" alt="start" >
+</a>
+ 
 
 <?php
 include "includes/footer.php";
