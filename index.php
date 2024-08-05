dba8870db714639cfed579fd8c99071d3636e881
Ngan <nganbui0330@gmail.com>
2024-08-05 11:31:04 -0400

    #

--- index.php
+++ index.php
@@ -4,12 +4,12 @@ include "includes/header.php";
 
 <img src="images/WorldMap.png" alt="world map" usemap="#worldmap">
     <map name="worldmap" id="worldmap" >
-        <area coords="130, 20, 500, 200" href="region.php?region_id=21" shape="rect">
-        <area coords="200, 200, 450, 450" href="region.php?region_id=13" shape="rect">
-        <area coords="470, 150, 650, 420" href="region.php?region_id=15" shape="rect">
-        <area coords="670, 40, 920, 290" href="region.php?region_id=30" shape="rect">
-        <area coords="850, 300, 990,500" href="region.php?region_id=9" shape="rect">
-        <area coords="500, 15, 675, 150" href="region.php?region_id=151" shape="rect">
+        <area coords="130, 20, 500, 200" href="#" shape="rect">
+        <area coords="200, 200, 450, 450" href="#" shape="rect">
+        <area coords="470, 150, 650, 420" href="#" shape="rect">
+        <area coords="670, 40, 920, 290" href="#" shape="rect">
+        <area coords="850, 300, 990,500" href="#" shape="rect">
+        <area coords="500, 15, 675, 150" href="#" shape="rect">
     </map>
 
 <?php
