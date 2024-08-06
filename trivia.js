2f0bab95339f190fa8abd7bc61efe49e1ab61616
jenny-129 <jenny3ward@gmail.com>
2024-08-06 09:22:52 -0400

    Add files via upload

--- trivia.js
+++ trivia.js
@@ -1,7 +1,8 @@
-let guesses = 0;
+let guesses = 3;
 let coins = 10;
 let continent = ""
 let x;
+let win = false;
 
 //get a random number between 1 and 7
 
@@ -44,32 +45,57 @@ function pickContinent(){
     alert(continent);
 }
 
-// pickContinent();
+pickContinent();
 
 //connect clickable regions to if statements
 
 function clickRegion(area){
     if (area == 'northAmerica' && continent == 'northAmerica'){
         alert("correct!")
+        win = true
     }
     else if (area == 'southAmerica' && continent == 'southAmerica'){
         alert("correct!")
+        win = true
     }
     else if (area == 'australia' && continent == 'australia'){
         alert("correct!")
+        win = true
     }
-    else if (area == 'antartica' && continent == 'antartica'){
+    else if (area == 'antarctica' && continent == 'antarctica'){
         alert("correct!")
+        win = true
     }
     else if (area == 'asia' && continent == 'asia'){
         alert("correct!")
+        win = true
     }
     else if (area == 'europe' && continent == 'europe'){
         alert("correct!")
+        win = true
     }
     else if (area == 'africa' && continent == 'africa'){
         alert("correct!")
+        win = true
     }
+    else{
+        alert("incorrect!")
+        win = false;
+        guesses -= 1;
+    }
+
+    if (win == true && guesses == 3){
+        coins += 5
+    }
+    else if (win == true && guesses == 2){
+        coins += 3
+    }
+    else if (win == true && guesses == 1){
+        coins += 2
+    }
+
+alert(`guesses: ${guesses}`)
+alert(`coins: ${coins}`)
 
 }
 
