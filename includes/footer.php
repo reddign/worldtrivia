b3087cd5edbb5f2ea3b02270c444ed35c6d61390
Ngan <nganbui0330@gmail.com>
2024-08-06 09:43:33 -0400

    ,

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,13 @@
 <br><br><br><br>
-<div style="text-align: center; color: lightGray; font-size: small;">
+<div style="text-align: center; color: darkGray; font-size: small;">
 Copyright MCA 2024
+<h6>
+Ngan Bui,
+Camila Urena,
+Camille Do,
+Jenny Ward
+</h6>
+
 </div>
 </body>
 </html>
