c9b747e529a8a5bd76ec67ac203f430d00e5a419
Ngan <buin@etown.edu>
2024-08-07 09:18:03 -0400

    gu

--- includes/header.php
+++ includes/header.php
@@ -12,8 +12,8 @@
           <!-- Right-sided navbar links. Hide them on small screens -->
           <div class="w3-right w3-hide-small">
             <a href="store.php" class="w3-bar-item w3-button">Store</a>
-            <a href="#" class="w3-bar-item w3-button">Guesses</a>
-            <a href="#contact" class="w3-bar-item w3-button">Coins</a>
+            <p class="w3-bar-item w3-button">Guesses</p>
+            <p class="w3-bar-item w3-button">Coins</p>
           </div>
         </div>
       </div>
