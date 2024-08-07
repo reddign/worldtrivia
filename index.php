66ba3f5e6c12a65bbbae1598cd69992178c2668c
Camila <urenacamila0@gmail>
2024-08-07 09:36:03 -0400

    hover

--- index.php
+++ index.php
@@ -2,13 +2,21 @@
 include "includes/header.php";
 ?>
 
-<br><br><br>
+<style>
+    a:hover{
+                background-color: lightblue;
+                width: 100px;
+            }
+</style>
+
+<br><br><br><br><br><br><br><br><br><br>
 
 <a href="map.php" >
-    <img id="start" src="images/startbutton.png" alt="start" style="border-style: none;">
+    <img id="start" src="images/startbutton.png" alt="start" style="border-style: none;" width="200px">
 </a>
  
 
+<br><br><br><br><br><br><br>
 <?php
 include "includes/footer.php";
 ?>
